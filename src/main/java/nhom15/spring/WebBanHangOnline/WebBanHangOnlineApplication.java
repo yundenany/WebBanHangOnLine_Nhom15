2777f0d66bc166343f2da3a43baf6fbc25206816
Vinh Hoàng <yundenany@gmail.com>
2023-05-17 13:01:30 +0700

    first code

---
+++ src/main/java/nhom15/spring/WebBanHangOnline/WebBanHangOnlineApplication.java
@@ -0,0 +1,13 @@
+package nhom15.spring.WebBanHangOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanHangOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanHangOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhom15/spring/WebBanHangOnline/WebBanHangOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package nhom15.spring.WebBanHangOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanHangOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
